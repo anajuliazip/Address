1fdf14abe6f1495b2113ad948a8d51df34f621ea
Ana Julia da Cruz <anajulianasci02@gmail.com>
2024-10-04 08:27:13 -0300

    Adicionar arquivos de projeto.

---
+++ Address/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Address
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txbDestinatario = new TextBox();
+            label1 = new Label();
+            btnEnviar = new Button();
+            label2 = new Label();
+            txbTipoLog = new TextBox();
+            label3 = new Label();
+            txbNomeLog = new TextBox();
+            label4 = new Label();
+            txbNumero = new TextBox();
+            label5 = new Label();
+            txbComplemento = new TextBox();
+            label6 = new Label();
+            txbBairro = new TextBox();
+            label7 = new Label();
+            txbCep = new TextBox();
+            label8 = new Label();
+            txbCidade = new TextBox();
+            label9 = new Label();
+            txbEstado = new TextBox();
+            label10 = new Label();
+            SuspendLayout();
+            // 
+            // txbDestinatario
+            // 
+            txbDestinatario.Location = new Point(110, 169);
+            txbDestinatario.Name = "txbDestinatario";
+            txbDestinatario.Size = new Size(100, 23);
+            txbDestinatario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(101, 149);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nome do destinatario";
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.Location = new Point(361, 316);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(106, 42);
+            btnEnviar.TabIndex = 2;
+            btnEnviar.Text = "Enviar";
+            btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(232, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Tipo do logradouro";
+            // 
+            // txbTipoLog
+            // 
+            txbTipoLog.Location = new Point(240, 169);
+            txbTipoLog.Name = "txbTipoLog";
+            txbTipoLog.Size = new Size(100, 23);
+            txbTipoLog.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(358, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Nome do logradouro";
+            // 
+            // txbNomeLog
+            // 
+            txbNomeLog.Location = new Point(367, 169);
+            txbNomeLog.Name = "txbNomeLog";
+            txbNomeLog.Size = new Size(100, 23);
+            txbNomeLog.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(497, 149);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Número";
+            // 
+            // txbNumero
+            // 
+            txbNumero.Location = new Point(497, 169);
+            txbNumero.Name = "txbNumero";
+            txbNumero.Size = new Size(100, 23);
+            txbNumero.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(626, 149);
+            label5.Name = "label5";
+            label5.Size = new Size(84, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Complemento";
+            // 
+            // txbComplemento
+            // 
+            txbComplemento.Location = new Point(626, 169);
+            txbComplemento.Name = "txbComplemento";
+            txbComplemento.Size = new Size(100, 23);
+            txbComplemento.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(177, 210);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Bairro";
+            // 
+            // txbBairro
+            // 
+            txbBairro.Location = new Point(177, 228);
+            txbBairro.Name = "txbBairro";
+            txbBairro.Size = new Size(100, 23);
+            txbBairro.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(304, 210);
+            label7.Name = "label7";
+            label7.Size = new Size(28, 15);
+            label7.TabIndex = 14;
+            label7.Text = "CEP";
+            // 
+            // txbCep
+            // 
+            txbCep.Location = new Point(304, 228);
+            txbCep.Name = "txbCep";
+            txbCep.Size = new Size(100, 23);
+            txbCep.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(434, 210);
+            label8.Name = "label8";
+            label8.Size = new Size(44, 15);
+            label8.TabIndex = 16;
+            label8.Text = "Cidade";
+            // 
+            // txbCidade
+            // 
+            txbCidade.Location = new Point(434, 228);
+            txbCidade.Name = "txbCidade";
+            txbCidade.Size = new Size(100, 23);
+            txbCidade.TabIndex = 15;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(563, 210);
+            label9.Name = "label9";
+            label9.Size = new Size(42, 15);
+            label9.TabIndex = 18;
+            label9.Text = "Estado";
+            // 
+            // txbEstado
+            // 
+            txbEstado.Location = new Point(563, 228);
+            txbEstado.Name = "txbEstado";
+            txbEstado.Size = new Size(100, 23);
+            txbEstado.TabIndex = 17;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label10.Location = new Point(239, 37);
+            label10.Name = "label10";
+            label10.Size = new Size(369, 31);
+            label10.TabIndex = 19;
+            label10.Text = "Informações de endereçamento";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(txbEstado);
+            Controls.Add(label8);
+            Controls.Add(txbCidade);
+            Controls.Add(label7);
+            Controls.Add(txbCep);
+            Controls.Add(label6);
+            Controls.Add(txbBairro);
+            Controls.Add(label5);
+            Controls.Add(txbComplemento);
+            Controls.Add(label4);
+            Controls.Add(txbNumero);
+            Controls.Add(label3);
+            Controls.Add(txbNomeLog);
+            Controls.Add(label2);
+            Controls.Add(txbTipoLog);
+            Controls.Add(btnEnviar);
+            Controls.Add(label1);
+            Controls.Add(txbDestinatario);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txbDestinatario;
+        private Label label1;
+        private Button btnEnviar;
+        private Label label2;
+        private TextBox txbTipoLog;
+        private Label label3;
+        private TextBox txbNomeLog;
+        private Label label4;
+        private TextBox txbNumero;
+        private Label label5;
+        private TextBox txbComplemento;
+        private Label label6;
+        private TextBox txbBairro;
+        private Label label7;
+        private TextBox txbCep;
+        private Label label8;
+        private TextBox txbCidade;
+        private Label label9;
+        private TextBox txbEstado;
+        private Label label10;
+    }
+}
