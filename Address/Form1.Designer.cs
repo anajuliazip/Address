a93da512b2226372ea04c98e28fde2a495ccbcd7
Ana Julia da Cruz <anajulianasci02@gmail.com>
2024-10-04 09:02:52 -0300

    Projeto de endereçamento, onde salva tanto no banco de dados, quanto em um arquivo

--- Address/Form1.Designer.cs
+++ Address/Form1.Designer.cs
@@ -52,162 +52,204 @@
             // 
             // txbDestinatario
             // 
-            txbDestinatario.Location = new Point(110, 169);
+            txbDestinatario.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbDestinatario.Font = new Font("Dutch801 Rm BT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txbDestinatario.Location = new Point(85, 201);
             txbDestinatario.Name = "txbDestinatario";
-            txbDestinatario.Size = new Size(100, 23);
+            txbDestinatario.Size = new Size(100, 22);
             txbDestinatario.TabIndex = 0;
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label1.AutoSize = true;
-            label1.Location = new Point(101, 149);
+            label1.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(63, 180);
             label1.Name = "label1";
-            label1.Size = new Size(122, 15);
+            label1.Size = new Size(148, 18);
             label1.TabIndex = 1;
             label1.Text = "Nome do destinatario";
             // 
             // btnEnviar
             // 
-            btnEnviar.Location = new Point(361, 316);
+            btnEnviar.Anchor = AnchorStyles.Top;
+            btnEnviar.FlatStyle = FlatStyle.Popup;
+            btnEnviar.Font = new Font("Dutch801 XBd BT", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEnviar.Location = new Point(346, 347);
             btnEnviar.Name = "btnEnviar";
             btnEnviar.Size = new Size(106, 42);
             btnEnviar.TabIndex = 2;
             btnEnviar.Text = "Enviar";
             btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.Click += btnEnviar_Click;
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label2.AutoSize = true;
-            label2.Location = new Point(232, 149);
+            label2.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(217, 180);
             label2.Name = "label2";
-            label2.Size = new Size(109, 15);
+            label2.Size = new Size(132, 18);
             label2.TabIndex = 4;
             label2.Text = "Tipo do logradouro";
             // 
             // txbTipoLog
             // 
-            txbTipoLog.Location = new Point(240, 169);
+            txbTipoLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbTipoLog.Font = new Font("Dutch801 Rm BT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txbTipoLog.Location = new Point(234, 200);
             txbTipoLog.Name = "txbTipoLog";
-            txbTipoLog.Size = new Size(100, 23);
+            txbTipoLog.Size = new Size(100, 22);
             txbTipoLog.TabIndex = 3;
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label3.AutoSize = true;
-            label3.Location = new Point(358, 149);
+            label3.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(355, 179);
             label3.Name = "label3";
-            label3.Size = new Size(119, 15);
+            label3.Size = new Size(141, 18);
             label3.TabIndex = 6;
             label3.Text = "Nome do logradouro";
             // 
             // txbNomeLog
             // 
-            txbNomeLog.Location = new Point(367, 169);
+            txbNomeLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbNomeLog.Font = new Font("Dutch801 Rm BT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txbNomeLog.Location = new Point(372, 201);
             txbNomeLog.Name = "txbNomeLog";
-            txbNomeLog.Size = new Size(100, 23);
+            txbNomeLog.Size = new Size(100, 22);
             txbNomeLog.TabIndex = 5;
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label4.AutoSize = true;
-            label4.Location = new Point(497, 149);
+            label4.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(533, 180);
             label4.Name = "label4";
-            label4.Size = new Size(51, 15);
+            label4.Size = new Size(60, 18);
             label4.TabIndex = 8;
             label4.Text = "Número";
             // 
             // txbNumero
             // 
-            txbNumero.Location = new Point(497, 169);
+            txbNumero.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbNumero.Font = new Font("Dutch801 Rm BT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txbNumero.Location = new Point(512, 200);
             txbNumero.Name = "txbNumero";
-            txbNumero.Size = new Size(100, 23);
+            txbNumero.Size = new Size(100, 22);
             txbNumero.TabIndex = 7;
+            txbNumero.TextChanged += txbNumero_TextChanged;
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Top;
             label5.AutoSize = true;
-            label5.Location = new Point(626, 149);
+            label5.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(630, 179);
             label5.Name = "label5";
-            label5.Size = new Size(84, 15);
+            label5.Size = new Size(98, 18);
             label5.TabIndex = 10;
             label5.Text = "Complemento";
             // 
             // txbComplemento
             // 
-            txbComplemento.Location = new Point(626, 169);
+            txbComplemento.Anchor = AnchorStyles.Top;
+            txbComplemento.Font = new Font("Dutch801 Rm BT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txbComplemento.Location = new Point(630, 200);
             txbComplemento.Name = "txbComplemento";
-            txbComplemento.Size = new Size(100, 23);
+            txbComplemento.Size = new Size(100, 22);
             txbComplemento.TabIndex = 9;
             // 
             // label6
             // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label6.AutoSize = true;
-            label6.Location = new Point(177, 210);
+            label6.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(180, 241);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(50, 18);
             label6.TabIndex = 12;
             label6.Text = "Bairro";
             // 
             // txbBairro
             // 
-            txbBairro.Location = new Point(177, 228);
+            txbBairro.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbBairro.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txbBairro.Location = new Point(162, 259);
             txbBairro.Name = "txbBairro";
-            txbBairro.Size = new Size(100, 23);
+            txbBairro.Size = new Size(100, 25);
             txbBairro.TabIndex = 11;
             // 
             // label7
             // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label7.AutoSize = true;
-            label7.Location = new Point(304, 210);
+            label7.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(321, 241);
             label7.Name = "label7";
-            label7.Size = new Size(28, 15);
+            label7.Size = new Size(37, 18);
             label7.TabIndex = 14;
             label7.Text = "CEP";
             // 
             // txbCep
             // 
-            txbCep.Location = new Point(304, 228);
+            txbCep.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbCep.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txbCep.Location = new Point(289, 259);
             txbCep.Name = "txbCep";
-            txbCep.Size = new Size(100, 23);
+            txbCep.Size = new Size(100, 25);
             txbCep.TabIndex = 13;
             // 
             // label8
             // 
+            label8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label8.AutoSize = true;
-            label8.Location = new Point(434, 210);
+            label8.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(443, 241);
             label8.Name = "label8";
-            label8.Size = new Size(44, 15);
+            label8.Size = new Size(53, 18);
             label8.TabIndex = 16;
             label8.Text = "Cidade";
             // 
             // txbCidade
             // 
-            txbCidade.Location = new Point(434, 228);
+            txbCidade.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbCidade.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txbCidade.Location = new Point(419, 259);
             txbCidade.Name = "txbCidade";
-            txbCidade.Size = new Size(100, 23);
+            txbCidade.Size = new Size(100, 25);
             txbCidade.TabIndex = 15;
             // 
             // label9
             // 
+            label9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label9.AutoSize = true;
-            label9.Location = new Point(563, 210);
+            label9.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(569, 241);
             label9.Name = "label9";
-            label9.Size = new Size(42, 15);
+            label9.Size = new Size(53, 18);
             label9.TabIndex = 18;
             label9.Text = "Estado";
             // 
             // txbEstado
             // 
-            txbEstado.Location = new Point(563, 228);
+            txbEstado.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            txbEstado.Font = new Font("Dutch801 Rm BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            txbEstado.Location = new Point(548, 259);
             txbEstado.Name = "txbEstado";
-            txbEstado.Size = new Size(100, 23);
+            txbEstado.Size = new Size(100, 25);
             txbEstado.TabIndex = 17;
             // 
             // label10
             // 
+            label10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             label10.AutoSize = true;
             label10.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            label10.Location = new Point(239, 37);
+            label10.Location = new Point(217, 70);
             label10.Name = "label10";
             label10.Size = new Size(369, 31);
             label10.TabIndex = 19;
@@ -217,6 +259,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.BurlyWood;
             ClientSize = new Size(800, 450);
             Controls.Add(label10);
             Controls.Add(label9);
--- Address/Form1.cs
+++ Address/Form1.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+
 namespace Address
 {
     public partial class Form1 : Form
@@ -10,6 +12,72 @@ namespace Address
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void txbNumero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            string connection = "server=127.0.0.1;userid=root;password=root;database=address";
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            {
+                try
+                {
+                    conn.Open();
+                    if (txbDestinatario.Text != null)
+                    {
+                        MySqlCommand cmd = new MySqlCommand("INSERT INTO `information` (`Destinatario`,`TipoLog`, `NomeLog`, `Number`, `Complemento`, `Bairro`, `CEP`, `Cidade`, `Estado`) VALUES (@Destinatario, @TipoLog, @NomeLog, @Number, @Complemento, @Bairro, @CEP, @Cidade, @Estado);", conn);
+                        cmd.Parameters.AddWithValue("@Destinatario", txbDestinatario.Text);
+                        cmd.Parameters.AddWithValue("@TipoLog", txbTipoLog.Text);
+                        cmd.Parameters.AddWithValue("@NomeLog", txbNomeLog.Text);
+                        cmd.Parameters.AddWithValue("@Number", txbNumero.Text);
+                        cmd.Parameters.AddWithValue("@Complemento", txbComplemento.Text);
+                        cmd.Parameters.AddWithValue("@Bairro", txbBairro.Text);
+                        cmd.Parameters.AddWithValue("@CEP", txbCep.Text);
+                        cmd.Parameters.AddWithValue("@Cidade", txbCidade.Text);
+                        cmd.Parameters.AddWithValue("@Estado", txbEstado.Text);
+
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+                    using (SaveFileDialog salvarArquivo = new SaveFileDialog())
+                    {
+                        salvarArquivo.Filter = "Arquivos de Texto (*.txt)|*.txt|Todos os Arquivos (*.*)|*.*";
+                        salvarArquivo.Title = "Salvar Arquivo";
+
+                        if (salvarArquivo.ShowDialog() == DialogResult.OK)
+                        {
+                            File.WriteAllText(salvarArquivo.FileName, "Destinatario: " + txbDestinatario.Text + "\n" + "Tipo do Logradouro: " + txbTipoLog.Text + "\n" + "Nome do Logradouro: " + txbNomeLog.Text + "\n" + "Número: " + txbNumero.Text + "\n" + "Complemento: " + txbComplemento.Text
+                                + "\n" + "Bairro: " + txbBairro.Text + "\n" + "CEP:: " + txbCep.Text + "\n" + "Cidade: " + txbCidade.Text + "\n" + "Estado: " + txbEstado.Text + "\n");
+
+                            MessageBox.Show("Você salvou seu arquivo.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao atualizar o registro: " + ex.Message);
+                }
+                finally
+                {
+                    txbDestinatario.Text = null;
+                    txbTipoLog.Text = null;
+                    txbNomeLog.Text = null;
+                    txbNumero.Text = null;
+                    txbNumero.Text = null;
+                    txbComplemento.Text = null;
+                    txbBairro.Text = null;
+                    txbCep.Text = null;
+                    txbCidade.Text = null;
+                    txbEstado.Text = null;
+
+                }
+            }
         }
     }
 }
